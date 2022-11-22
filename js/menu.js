c69cba8d9000d954a413ec2f0ec8851e91d94e52
Ksenia Fihas <ksenia.fihas@gmail.com>
2022-11-22 11:03:23 +0100

    start mob-header

--- js/menu.js
+++ js/menu.js
@@ -1,23 +1,14 @@
 (() => {
-  const refs = {
-    openMenuBtn: document.querySelector('[data-menu-open]'),
-    closeMenuBtn: document.querySelector('[data-menu-close]'),
-    menu: document.querySelector('[data-menu]'),
-    body: document.querySelector('body'),
-    menuList: document.querySelector('.mob-menu-list'),
-  };
+  const menuBtnRef = document.querySelector('[data-menu-button]');
 
-  refs.openMenuBtn.addEventListener('click', toggleMenu);
-  refs.closeMenuBtn.addEventListener('click', toggleMenu);
-  refs.menuList.addEventListener('click', removeMenu);
 
-  function toggleMenu() {
-    refs.menu.classList.toggle('is-hidden');
-    refs.body.classList.toggle('no-scroll');
-  }
+  menuBtnRef.addEventListener('click', () => {
+    const expanded =
+      menuBtnRef.getAttribute('aria-expanded') === "true" || false;
+    
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
 
-  function removeMenu() {
-    refs.menu.classList.add('is-hidden');
-    refs.body.classList.remove('no-scroll');
-  }
+    mobileMenuRef.classList.toggle("is-open");
+  });
 })();
